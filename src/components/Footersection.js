4c23d3da634d7b5435884255da776ccba92fe0c9
Chinmay-coder-10 <chinmaybhogaonkarb@gmail.com>
2022-02-13 22:41:24 +0530

    Completed Footer section

--- src/components/Footersection.js
+++ src/components/Footersection.js
@@ -5,16 +5,17 @@ import Register from './Register';
 const Footersection = () => {
     return (
         <div style={{ margin: "4rem" }}>
-            <h1 style={{fontSize:"50px"}} className="text-center my-5 heading">Start saving notes</h1>
-            <div style={{ display: "flex" }} className="footeritem">
+            <h1 style={{ fontSize: "50px" }} className="text-center my-5 heading footerheading">Start saving notes</h1>
+            <div className="footeritem">
                 <div className="leftside">
                     <h1 className="getstarted" >Get started</h1>
                     <p className="getstartedp">By creating an account you can start saving notes</p>
                 </div>
                 <div className="rightside">
-                    <Register/>
+                    <Register />
                 </div>
             </div>
+            <div className="notecard"></div>
         </div>
     )
 };
